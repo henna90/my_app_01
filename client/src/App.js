e3e34581a66d87bbed5afcbb82fb5f1e1132f0aa
Henna Abbas <hennaabbas@hennas-mbp.lan>
2020-01-28 19:22:12 -0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+import Button from './Button'
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+     Landing Page 
+      </header>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+cont path = require('path')
+// Create the server
+const app = express()
+// Serve our api route /cow that returns a custom talking text cow
+app.get('/test-route', cors(), async (req, res) => {
+  try {
+    res.json("this route is working ")
+  } catch (err) {
+    next(err)
+  }
+})
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Mixing it up on port ${PORT}`)
+})
