8a502ad1bc6e138fb5b3787acbd48bd0302fd5a5
Henna Abbas <hennaabbas@hennas-mbp.lan>
2020-01-28 19:54:12 -0800

    updated tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import './App.css';
+import React from 'react';
 import Button from './Button'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header >
-     Landing Page 
-      </header>
-      <Button />
+          <h1>My test app</h1>
+      <Button/>
     </div>
   );
 }
